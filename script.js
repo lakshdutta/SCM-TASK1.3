e92bf6e37699b941fbabf60a3cce9358ea4bc23e
Nishchaya <nish3654@gmail.com>
2024-05-16 15:45:15 -0700

    play pause button is fixed

--- script.js
+++ script.js
@@ -29,37 +29,37 @@ songItems.forEach((element, i)=>{
  
 
 // Handle play/pause click
-// masterPlay.addEventListener('click', ()=>{
-//     if(audioElement.paused || audioElement.currentTime<=0){
-//         audioElement.play();
-//         masterPlay.classList.remove('fa-play-circle');
-//         masterPlay.classList.add('fa-pause-circle');
-//         gif.style.opacity = 1;
-//     }
-//     else{
-//         audioElement.pause();
-//         masterPlay.classList.remove('fa-pause-circle');
-//         masterPlay.classList.add('fa-play-circle');
-//         gif.style.opacity = 0;
-//     }
-// })
-// // Listen to Events
-// audioElement.addEventListener('timeupdate', ()=>{ 
-//     // Update Seekbar
-//     progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
-//     myProgressBar.value = progress;
-// })
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
 
-// myProgressBar.addEventListener('change', ()=>{
-//     audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
-// })
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
 
-// const makeAllPlays = ()=>{
-//     Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-//         element.classList.remove('fa-pause-circle');
-//         element.classList.add('fa-play-circle');
-//     })
-// }
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
 
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{ 
